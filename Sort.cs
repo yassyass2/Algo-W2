a60e45660ac3bf6f23d0cb0848d6385482bdfc89
yassyass2 <yassinabde@outlook.com>
2025-02-18 11:00:41 +0100

    implemented bubble sort

--- Sort.cs
+++ Sort.cs
@@ -3,7 +3,20 @@ public class Sort<T> : ISort<T> where T : IComparable<T>
 {
     public static void BubbleSort(T[] data)
     {
-        throw new NotImplementedException(); //ToDo
+        if (data == null || data.Length <= 1) return;
+        bool swap = true;
+        int until = 1;
+        while(swap){
+            swap = false;
+            for (int i = 0; i < data.Length-until; i++){
+                if (data[i] > data[i+1]){
+                    (data[i], data[i+1]) = (data[i+1], data[i]);
+                    swap = true;
+                }
+            }
+            until++;
+        }
+
     }
 
     public static void InsertionSort(T[] data)
