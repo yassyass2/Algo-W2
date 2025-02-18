77d7d070955c6932497d97c96bc0970a3dd82806
yassyass2 <yassinabde@outlook.com>
2025-02-18 10:46:58 +0100

    added template files

---
+++ Sort.cs
@@ -0,0 +1,24 @@
+namespace ToDo;
+public class Sort<T> : ISort<T> where T : IComparable<T>
+{
+    public static void BubbleSort(T[] data)
+    {
+        throw new NotImplementedException(); //ToDo
+    }
+
+    public static void InsertionSort(T[] data)
+    {
+        throw new NotImplementedException(); //ToDo
+    }
+
+
+    public static void MergeSort(T[] array, int low, int high)
+    {
+        throw new NotImplementedException(); //ToDo
+    }
+
+    public static void Merge(T[] array, int low, int middle, int high)
+    {
+        throw new NotImplementedException(); //ToDo
+    }
+}
