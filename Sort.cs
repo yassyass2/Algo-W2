8238271374fc3583df8f26982f7f96a1f63ae4c5
yassyass2 <yassinabde@outlook.com>
2025-02-27 23:00:43 +0100

    removed continu

--- Sort.cs
+++ Sort.cs
@@ -66,11 +66,9 @@ public class Sort<T> : ISort<T> where T : IComparable<T>
         for (int k = low; k <= high; k++){
             if (low >= firstArr) { 
                 array[k] = rightArr[secondBegin++]; 
-                continue;
             } 
             else if (secondBegin >= secondArr || rightArr[secondBegin].CompareTo(leftArr[low]) >= 0){
                 array[k] = leftArr[low++];
-                continue;
             }
             else if (rightArr[secondBegin].CompareTo(leftArr[low]) < 0){
                 array[k] = rightArr[secondBegin++];
